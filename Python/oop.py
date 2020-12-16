195ed639543f42e8af1a45877ff2c835f30695fb
Xolo <solomonmanyaga@gmail.com>
2020-12-16 15:22:44 -0500

    OOP

---
+++ Python/oop.py
@@ -0,0 +1,45 @@
+# naming is CamelCase instead of snake
+# name of class is always singular
+# in classes the default always when we write a method is self.
+# note you dont pass in the self as you are instanciating an instance
+# we can add attributes outside the class deffinition
+
+
+class PlayerCharacter:
+    # Class Object Attribute, exists for all instances and cant be changed
+    profession = 'Footballer'
+    def __init__(self, name):
+        self.name = name            # attributes / property 
+
+    def shout(self):                # note how we access class and instance attr
+        print(f"heeeyy I\'m {self.name} and I am a {PlayerCharacter.profession}")
+        # note this function returns None
+
+    # class method are created under decorator
+    # takes in cls inplace of self
+    # can be used without instanciating
+    @classmethod
+    def adding_things(cls,a,b):
+        return a + b
+
+    # static methods are aso created under decorator
+    # doesnt takes in cls nor self
+    # can be used without instanciating
+    @staticmethod
+    def multiplying_things(a,b):
+        return a * b
+
+player1 = PlayerCharacter('Kaka')
+player2 = PlayerCharacter('Scholes')
+
+print(PlayerCharacter.adding_things(1,2))
+
+print(player1.name)
+print(player2.name)
+
+# adding extra attributes
+player1.super_powers = 'Baking'
+print(player1.super_powers)
+
+# help gives you the blue print of an object
+print(player2.shout())
--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,32 @@
-a = 'abcdefghij'
+#Given the below class:
+class Cat:
+    species = 'mammal'
 
-while ((n := len(a)) > 0):
-    print(a)
-    a = a[:-1]
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('first', 1)
+cat2 = Cat('second', 28)
+cat3 = Cat('third', 3)
+
+# 2 Create a function that finds the oldest cat
+def find_oldest(*cats):
+    oldest = 0
+    for cat in cats:
+        if cat.age > oldest:
+            oldest = cat.age
+    
+    return oldest
+
+# dude you have max use it you nut head
+def get_oldest_cat(*catAges):
+    return max(catAges)
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+
+
+print(f"The oldest cat is {find_oldest(cat1,cat2,cat3)} years old.")
+print(f"The oldest cat is {get_oldest_cat(cat1.age, cat2.age, cat3.age) } years old.")
