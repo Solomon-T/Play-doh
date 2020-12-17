90b54b6a58ed67dcec4bfa761adf1dcaf7416df7
Xolo <solomonmanyaga@gmail.com>
2020-12-17 09:15:11 -0500

    pure funtions

---
+++ Python/funtional_prg.py
@@ -0,0 +1,10 @@
+# Funtional programming is all bout separation of concerns
+# that means each part conserns itself about one that its good at
+# separate data and funtions
+
+# Pure funtions
+    # same input same output
+    # should not have side effects 
+        # - changes nothing but input
+        # doenst care about the outside world
+        # different parts of the program doesnt touch each other
