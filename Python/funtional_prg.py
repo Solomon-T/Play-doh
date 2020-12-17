8145d84129afaa3bc78ae94d189fd93e8fe3b8d6
Xolo <solomonmanyaga@gmail.com>
2020-12-17 09:39:09 -0500

    map, filter, reduce, zip

--- Python/funtional_prg.py
+++ Python/funtional_prg.py
@@ -8,3 +8,33 @@
         # - changes nothing but input
         # doenst care about the outside world
         # different parts of the program doesnt touch each other
+
+# map
+    # creates a new map object that we have to convert to list
+    # input list is not modified
+my_list = [1, 2, 3]
+def multiply_by2(item):
+    return item*2
+# 
+print(list(map(multiply_by2, my_list)))
+
+# filter
+def isOdd(item):
+    return item % 2 != 0
+
+print(list(filter(isOdd, my_list)))
+
+# zip
+    # takes any two iterables doesnt have to be a list and can be more than two
+your_list = [99,87,63]
+print(list(zip(your_list, my_list)))
+
+# reduce
+    # doesnt come as a built in funtion
+    # return a list no need for conversion
+from functools import reduce
+
+def accumutator(acc, item):
+    return acc + item
+
+print(reduce(accumutator,list(my_list),0))
