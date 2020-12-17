1266994ea2a433db5e92e21d478cbacb67c94c25
Xolo <solomonmanyaga@gmail.com>
2020-12-17 09:06:18 -0500

    MRO

--- Python/oop.py
+++ Python/oop.py
@@ -3,6 +3,7 @@
 # in classes the default always when we write a method is self.
 # note you dont pass in the self as you are instanciating an instance
 # we can add attributes outside the class deffinition
+# isinstance(instance, Class) - checks if an item is the instance of a class.
 
 # Note the difference btn objects and dictionaries to access atrributes
     # for an object we use the dot notation 
@@ -34,21 +35,100 @@ class PlayerCharacter:
 
 player1 = PlayerCharacter('Kaka')
 player2 = PlayerCharacter('Scholes')
-
-print(PlayerCharacter.adding_things(1,2))
-
-print(player1.name)
-print(player2.name)
-
+# print(PlayerCharacter.adding_things(1,2))
+# print(player1.name)
+# print(player2.name)
 # adding extra attributes
 player1.super_powers = 'Baking'
-print(player1.super_powers)
-
+# print(player1.super_powers)
 # help gives you the blue print of an object
-print(player2.shout())
+# print(player2.shout())
 
 
 
 # 4 PILLARS OF OOP
     # Encapsulation - is the binding of data and funtions that munipulate it
     # Abstraction - hidding info and giving access only whats needed
+    # Inheritance - 
+    # Polymorphism - in python it referes to this idea that object classes can 
+                #    share the same method name but act differently
+
+# Public and Private
+    # no true pravacy in python
+    # we just do _name thats just agreed between programers
+    # __something belongs to python and should never be overwritten
+
+
+class User():
+    # Note we don't have to have __init__ if we dont need attributes
+    def __init__(self, email):
+        self.email = email
+    
+    def sign_in(self):
+        print('logged_in')
+
+# To inherit simply pass in the parent class in the brackets
+# Look at how to usr super in init of the __init__ for the Wizard class
+# To extend just add other methods to the class
+# It is possible to overide the parrent class
+class Wizard(User):
+    def __init__(self, name, power, email='none'):
+        User.__init__(self, email)
+        # super.__init__(email)   # a better and modern way
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'{self.name} attacking with the power of {self.power}')
+
+class Archer(User):
+    def __init__(self, name, number_of_arrows, email='none'):
+        User.__init__(self, email)
+        # super.__init__(email)
+        self.name = name
+        self.number_of_arrows = number_of_arrows
+
+    def attack(self):
+        print(f'{self.name} attacking with the number_of_arrows of {self.number_of_arrows}')
+
+wiz1 = Wizard('Masenga', 'Kabwela')
+wiz1.sign_in()
+wiz1.attack()
+
+archer = Archer('Xolo', 2534)
+archer.sign_in()
+archer.attack()
+
+
+# isinstance
+    # isinstance(istance, Class) => true
+    # isinstance(istance, ParentClass) => true
+    # isinstance(istance, Object) => true
+        # everything ingerits form the python object base class
+        # this is where all the dunder methods on everything come from
+
+# Multiple inheritance
+class Hybrid(Wizard, Archer):
+    def __init__(self, name, power, arrows):
+        Archer.__init__(self, name, arrows)
+        Wizard.__init__(self,name, power)
+
+hb = Hybrid('Xolo', 'ungo', 693)
+print(hb.number_of_arrows)
+
+
+# MRO - method resolution order - uses DFS under the hood
+class A:
+    num = 10
+
+class B(A):
+    pass
+
+class C(A):
+    num = 2
+
+class D(B,C):
+    pass
+
+print(D.num)
+print(D.mro())
--- Python/test.py
+++ Python/test.py
@@ -1,32 +1,14 @@
-#Given the below class:
-class Cat:
-    species = 'mammal'
+class SuperList(list):
+    def __len__(self):
+        return 1000
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-# 1 Instantiate the Cat object with 3 cats
-cat1 = Cat('first', 1)
-cat2 = Cat('second', 28)
-cat3 = Cat('third', 3)
-
-# 2 Create a function that finds the oldest cat
-def find_oldest(*cats):
-    oldest = 0
-    for cat in cats:
-        if cat.age > oldest:
-            oldest = cat.age
-    
-    return oldest
-
-# dude you have max use it you nut head
-def get_oldest_cat(*catAges):
-    return max(catAges)
-
-# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+super_list = SuperList()
+print(super_list)
+print(len(super_list))
+print('-----------------')
+super_list.append('abc')
+super_list.append('ksa;')
+print(super_list)
+print(len(super_list))
 
 
-print(f"The oldest cat is {find_oldest(cat1,cat2,cat3)} years old.")
-print(f"The oldest cat is {get_oldest_cat(cat1.age, cat2.age, cat3.age) } years old.")
