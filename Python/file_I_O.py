2c9e89a1bf0c0eac1313a78c5d35bb2c3f8d7779
Xolo <solomonmanyaga@gmail.com>
2020-12-18 09:30:48 -0500

    file I/O

--- Python/file_I_O.py
+++ Python/file_I_O.py
@@ -1,3 +1,13 @@
 
+my_file = open('test.txt')
 
+print(my_file)
+print(my_file.read())
+my_file.seek(0)                 # takes the cursor back to the first line
+print('----------------------')
+print(my_file.readline())
 
+my_file.seek(0)
+print(my_file.readlines())
+
+my_file.close()
