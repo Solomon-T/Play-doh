dbbdb897cd7edff1b226b405cdf993eba15552c7
Xolo <solomonmanyaga@gmail.com>
2020-12-23 08:21:25 -0500

    lifeCycleMethods

--- TypeScript/Playground/robo-friends/src/App.js
+++ TypeScript/Playground/robo-friends/src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList'
 import { robots } from './robots'
+import SearchBox from './SearchBox'
+import './App.css'
 
-const App = () => {
-    return(
-        <div>
-            <h2>Robo dudes</h2>
-            <CardList robots={robots}/>
-        </div>
-    )
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+        console.log('constructor')
+    }
+
+    componentDidMount() {
+        this.setState({robots: robots})
+        console.log('componentDidMount')
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){    
+        const filteredRobots = this.state.robots.filter( robot => (
+            robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+        ))
+        console.log('render')
+        return(
+            <div className='tc'>
+                <h1 className='f2'>Robo dudes</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        )
+    }
 }
+
+// const App = () => {
+//     return(
+//         <div className='tc'>
+//             <h2>Robo dudes</h2>
+//             <SearchBox/>
+//             <CardList robots={robots}/>
+//         </div>
+//     )
+// }
 
 export default App;
