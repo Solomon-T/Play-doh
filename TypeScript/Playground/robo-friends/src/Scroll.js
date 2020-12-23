b26da5df781d719aa51d129cb02fbb5b8b828ae9
Xolo <solomonmanyaga@gmail.com>
2020-12-23 10:36:42 -0500

    adds scrolling

--- TypeScript/Playground/robo-friends/src/Scroll.js
+++ TypeScript/Playground/robo-friends/src/Scroll.js
@@ -2,8 +2,12 @@
 
 const Scroll = (props) => {
     // return <h1> Scrollable </h1>
-    return props.children
-    
+    // return props.children
+    return (
+        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+            {props.children}
+        </div>
+    )
 }
 
 export default Scroll;
