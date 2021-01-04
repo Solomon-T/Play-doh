9602c5b68b5c941acecf79cdb7ad84ac7e4ecf34
Xolo <solomonmanyaga@gmail.com>
2021-01-04 15:40:29 -0500

    notes

--- Python/mixtape.py
+++ Python/mixtape.py
@@ -4,3 +4,12 @@
 #  i ++ doesnt work in python use i += 1
 
 abs(-1)
+# abs  in python is just a function
+
+# how to do truthy and falsey  values in python
+# if node works
+# if !node doesnt  work
+
+# python is 'and' not '&&'
+# apppend in  list instead of push
+# None  instead of undiffined
