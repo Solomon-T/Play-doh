ee30a74be93746c5d2509473dcb9ab47fd9a0977
Xolo <solomonmanyaga@gmail.com>
2021-01-05 13:52:52 -0500

    notes

--- Python/mixtape.py
+++ Python/mixtape.py
@@ -13,3 +13,12 @@ abs(-1)
 # python is 'and' not '&&'
 # apppend in  list instead of push
 # None  instead of undiffined
+node = 'just a tree node'
+
+if node is None:
+    pass
+
+# for loops dont have () and they are not complicated
+
+# we dont have shift and unshift in list
+we use pop() or pop(0)
